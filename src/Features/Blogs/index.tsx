56bbaf2ff26dbdb0303b54652016ebf671d2184f
Michal Hazucha <hazuchamichal@gmail.com>
2020-12-04 11:17:05 +0100

    Updates

--- src/Features/Blogs/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-import MainBlogs from '../../components/MainBlogs';
-const Blogs = () => {
-  return <MainBlogs />;
-};
-
-export default Blogs;
